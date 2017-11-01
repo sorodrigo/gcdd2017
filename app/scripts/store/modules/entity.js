b54ccb8724051101d517fda8a48bc244bff7c74a
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-11-01 22:55:49 +0100

    feat: refactors specific use case modules into entity module

--- app/scripts/store/modules/entity.js
+++ app/scripts/store/modules/entity.js
@@ -1,38 +1,43 @@
 import {
-  SET_STAFF,
-  SET_STAFF_ERROR,
+  SET_CURRENT_ENTITY,
+  SET_ENTITY_LIST,
+  SET_ENTITY_ERROR,
 } from '../mutation-types';
 
 const staff = {
   // INITIAL STATE
   state: {
-    list: [],
+    data: {},
+    current: {},
     error: null,
   },
   // MUTATIONS
   mutations: {
-    [SET_STAFF](state, list) {
-      state.list = list;
+    [SET_CURRENT_ENTITY](state, current) {
+      state.current = current;
     },
-    [SET_STAFF_ERROR](state, error) {
+    [SET_ENTITY_LIST](state, list, entity) {
+      state.data = { ...state.data, [entity]: { ...state.data[entity], list } };
+    },
+    [SET_ENTITY_ERROR](state, error) {
       state.error = error;
     },
   },
   // ACTIONS
   actions: {
-    setStaff({ commit }) {
+    setStaff({ commit }, { entity }) {
       return new Promise((resolve, reject) => {
-        fetch('/api/staff')
+        fetch(`/api/${entity}`)
           .then((res) => {
             if (res.status >= 400) throw new Error(res.status);
             return res.json();
           })
           .then((data) => {
-            commit(SET_STAFF, data);
+            commit(SET_ENTITY_LIST, data);
             resolve(data);
           })
           .catch((err) => {
-            commit(SET_STAFF_ERROR, err);
+            commit(SET_ENTITY_ERROR, err);
             reject(err);
           });
       });
@@ -40,7 +45,7 @@ const staff = {
   },
   // GETTERS
   getters: {
-    getStaff: state => state.list,
+    getEntity: ({ data, current }) => data[current],
   },
 };
 
--- app/scripts/store/mutation-types.js
+++ app/scripts/store/mutation-types.js
@@ -1,26 +1,8 @@
-// professors
-export const SET_PROFESSORS = 'professors/SET_PROFESSORS';
-export const SET_PROFESSORS_ERROR = 'professors/SET_PROFESSORS_ERROR';
-
-// courses
-export const SET_COURSES = 'professors/SET_COURSES';
-export const SET_COURSES_ERROR = 'professors/SET_COURSES_ERROR';
-
-// degrees
-export const SET_DEGREES = 'professors/SET_DEGREES';
-export const SET_DEGREES_ERROR = 'professors/SET_DEGREES_ERROR';
-
-// categories
-export const SET_CATEGORIES = 'categories/SET_CATEGORIES';
-export const SET_CATEGORIES_ERROR = 'categories/SET_CATEGORIES_ERROR';
 
 // staff
-export const SET_STAFF = 'staff/SET_STAFF';
-export const SET_STAFF_ERROR = 'staff/SET_STAFF_ERROR';
-
-// occupations
-export const SET_OCCUPATIONS = 'occupations/SET_OCCUPATIONS';
-export const SET_OCCUPATIONS_ERROR = 'occupations/SET_OCCUPATIONS_ERROR';
+export const SET_CURRENT_ENTITY = 'entity/SET_CURRENT_ENTITY';
+export const SET_ENTITY_LIST = 'entity/SET_ENTITY_LIST';
+export const SET_ENTITY_ERROR = 'entity/SET_ENTITY_ERROR';
 
 // modal
 export const SET_MODAL = 'modal/SET_MODAL';
